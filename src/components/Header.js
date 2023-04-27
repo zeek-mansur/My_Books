eea792844e61a9ae461052a34d5ddcf195044823
Zeek <mzulkiful@yahoo.com>
2023-04-27 19:19:54 +0100

    Custom Hook added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react'
-//import {v4} from 'uuid'
+import { useFetch } from '../Hooks/useFetch'
+
 // styles
 import './Header.css'
-import SearchResults from './SearchResults'
 // components
-//import SearchResults from './SearchResults'
-
+import SearchResults from './SearchResults'
 
 
 export default function Header() {
 
   const [term , setTerm] = useState('')
   const [searchResults, setSearchResults] = useState([])
+  const { isPending, error} = useFetch(`https://www.googleapis.com/books/v1/volumes?q=${term}&key=AIzaSyAu2a9a18TPFSx-6Vo4RpK4AdpX8Hy8FBk`)
 
+  
 
   const getSearchResults = async () => {
       const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${term}&key=AIzaSyAu2a9a18TPFSx-6Vo4RpK4AdpX8Hy8FBk`)
@@ -22,14 +23,13 @@ export default function Header() {
     console.log(data.items)
   }
 
-// console.log(data)
+//console.log(data)
 
-  const handleKeyDown = (e)  => {
+const handleKeyDown = (e)  => {
   
       if(e.key=== 'Enter') {
         getSearchResults()
       }
-  
   }
   return (
     <div>
@@ -37,10 +37,14 @@ export default function Header() {
        <h1 className='name'>My Books</h1>
        <input onKeyDown={handleKeyDown} onChange={(e) => setTerm(e.target.value)} type='search' placeholder='Search Book Name' />  
     </div>
+    
      <main>
-     <ul className='search-result'>
+      
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {searchResults &&  <ul className='search-result'>
          {searchResults.map(each=> <SearchResults key ={each.id} item ={each} /> )}
-     </ul>
+     </ul>}
    </main>
    </div>
   )
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -8,14 +8,14 @@ const [Item,setItem]=useState();
 const [show,setShow]=useState(false)
 const {item} = props
 let author = item.volumeInfo.authors;
-let imageLink = item.volumeInfo.imageLinks.smallThumbnail;
+let imageLink = item.volumeInfo.imageLinks.thumbnail;
 let title = item.volumeInfo.subtitle;
 //console.log(bookDetails)
 
   return (
     <div>
       <li className='result'>
-          <img src={imageLink} alt = "title" onClick={()=>{setShow(true);setItem(item)}}/>
+      <img src={imageLink} alt = "title" onClick={()=>{setShow(true);setItem(item)}}/>
           <h3>Title: {title}</h3>
           <p>Author:{author}</p>
       </li>
